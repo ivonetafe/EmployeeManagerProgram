a2cfa6dd19c89033be7ac036ca2abffb6214eae6
Ivone <ivone.bennett@nmtafe.wa.edu.au>
2018-09-19 09:33:01 +0800

    Starter code for the employee manager program.

---
+++ src/employeemanagerprogram/Employee.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagerprogram;
+
+/**
+ * The employee class holds employee data
+ * @author 5002104
+ */
+public class Employee {
+    private String name;
+    private int employeeNum;
+
+    public Employee(String name, int employeeNum) {
+        this.name = name;
+        this.employeeNum = employeeNum;
+    }
+       
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeNum() {
+        return employeeNum;
+    }
+
+    public void setEmployeeNum(int employeeNum) {
+        this.employeeNum = employeeNum;
+    }
+    
+    public String printDetails() {
+        return employeeNum + " " + name;
+    }
+}
---
+++ src/employeemanagerprogram/EmployeeManagerProgram.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package employeemanagerprogram;
+
+/**
+ * This is an Employee Manager program under source control.
+ * @author 5002104
+ */
+public class EmployeeManagerProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Bob", 123);
+        System.out.println(emp1.printDetails());
+    }
+    
+}
